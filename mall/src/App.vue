a814d1df45101fdbe932e05cbeb29aa79d395f7b
tagzd123 <626228508@qq.com>
2020-08-23 15:52:09 +0800

    头部导航栏

--- mall/src/App.vue
+++ mall/src/App.vue
@@ -13,23 +13,16 @@ export default {
   },
   mounted(){
     //本地加载请求静态json文件的形式
-    this.axios.get('/user/login').then((res)=>{
-      this.res=res;
+    // this.axios.get('/user/login').then((res)=>{
+    //   this.res=res;
     //通过easy-mock
     //本地集成mockjs实现数据mock
   
-    });
+  //   });
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+@import './assets/scss/reset.scss'
 </style>
